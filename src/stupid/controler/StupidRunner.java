0e138adcf3625094c6bef53bf05e0159cb1d1d86
Brashear <pbra1484@740s-pl-108.local>
2016-09-06 13:53:34 -0600

    nothing

---
+++ src/stupid/controler/StupidRunner.java
@@ -0,0 +1,9 @@
+package stupid.controler;
+
+public class StupidRunner 
+{
+	public static void main (String [ ] args)
+	{
+		
+	}
+}
--- src/stupid/model/FirstStupid.java
+++ src/stupid/model/FirstStupid.java
@@ -1,5 +1,6 @@
 package stupid.model;
 
-public class FirstStupid {
+public class FirstStupid 
+{
 
 }
